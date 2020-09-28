c6f8f8afddbf84b9630ff43b96c36fc3469c7c64
arifincoding <arifinnakpojok@gmail.com>
2020-09-28 07:52:17 +0700

    belajar vue router

--- belajar framework vue/chapter 2/my-first-vue-cli-app/src/main.js
+++ belajar framework vue/chapter 2/my-first-vue-cli-app/src/main.js
@@ -4,8 +4,9 @@ import {
 import App from './App.vue'
 import ReUse from './Component.vue'
 import UseProps from './Prop.vue'
+import router from './router'
 
 
-createApp(App).mount('#app')
-createApp(ReUse).mount('#reUse')
-createApp(UseProps).mount('#useProps')
+createApp(App).use(router).mount('#app')
+createApp(ReUse).use(router).mount('#reUse')
+createApp(UseProps).use(router).mount('#useProps')
