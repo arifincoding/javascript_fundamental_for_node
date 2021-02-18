27d317663294d2bb191ec104376befecc5a791bc
arifincoding <arifinnakpojok@gmail.com>
2021-02-18 16:44:47 +0700

    learn react

--- belajar react/e-commerce/src/App.js
+++ belajar react/e-commerce/src/App.js
@@ -12,8 +12,8 @@ function App(){
         <div className="jumbo-tron">
         </div>
         <div className="jumbotron-content">
-        <nav className="navbar navbar-expand-lg navbar-dark">
-        <div className="navbar-brand">Pasarku</div>
+        <nav className="navbar container navbar-expand-lg navbar-dark">
+        <div className="navbar-brand font-weight-bold">Pasarku</div>
         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
@@ -27,24 +27,40 @@ function App(){
           </div>
         </nav>
         <div className="d-lg-flex d-none justify-content-center mt-4">
-            <div className="col-8 row bg-light rounded-pill">
-              <div className="my-2 col-4 border-right">
-              <label className="font-weight-bold m-0">Lokasi</label>
+            <div className="col-8 row pr-1 bg-light rounded-pill">
+              <div className="my-1 col-4 border-right">
+              <label className="font-weight-bold m-0 small">Lokasi</label>
               <input className="form-control border-0 p-0 form-control-sm" type="text" placeholder="Dimanakah Anda ?"/>
             </div>
-            <div className="my-2 col-3 border-right">
-              <label className="font-weight-bold m-0">Lokasi</label>
+            <div className="my-1 col-3 border-right">
+              <label className="font-weight-bold m-0 small">Tanggal Kirim</label>
               <input className="form-control border-0 p-0 form-control-sm" type="text" placeholder="Dimanakah Anda ?"/>
             </div>
-            <div className="my-2 col-4 border-right">
-              <label className="font-weight-bold m-0">Lokasi</label>
+            <div className="my-1 col-3">
+              <label className="font-weight-bold m-0 small">Jam Kirim</label>
               <input className="form-control border-0 p-0 form-control-sm" type="text" placeholder="Dimanakah Anda ?"/>
             </div>
-            <div className="my-2 col-1 pl-1 pr-0 text-right">
-              <button className="btn py-3 px-4 btn-danger rounded-circle"><i className="fas fa-search"></i></button>
+            <div className="my-1 col pl-1 pr-0 text-right">
+              <button className="btn btn-danger btn-search rounded-circle"><i className="fas fa-search"></i></button>
             </div>
           </div>
         </div>
+        <div className="find-place container">
+          <h1 className="text-white font-weight-bold">Cari<br/>Yang Dekat</h1>
+          <button className="btn btn-light btn-sm mt-2 font-weight-bold">telusuri pasar disekitar</button>
+        </div>
+        </div>
+        <div className="mt-5 container">
+          <h2 className="font-weight-bold">Cari kategori apa?</h2>
+          <h2 className="font-weight-bold">Cari kategori apa?</h2>
+          <h2 className="font-weight-bold">Cari kategori apa?</h2>
+          <h2 className="font-weight-bold">Cari kategori apa?</h2>
+          <h2 className="font-weight-bold">Cari kategori apa?</h2>
+          <h2 className="font-weight-bold">Cari kategori apa?</h2>
+          <h2 className="font-weight-bold">Cari kategori apa?</h2>
+          <h2 className="font-weight-bold">Cari kategori apa?</h2>
+          <h2 className="font-weight-bold">Cari kategori apa?</h2>
+          <h2 className="font-weight-bold">Cari kategori apa?</h2>
         </div>
       </div>
     );
